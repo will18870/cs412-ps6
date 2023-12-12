77d0187dc9085a446014d971ff9eb337ed498c1b
will18870 <will18870@live.com>
2023-12-11 19:56:51 -0500

    did the thing!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,36 @@
+// William Huang - U53888747
+
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet></router-outlet>
-  `,
-  styles: [],
+  templateUrl: './app.component.html',
+  imports: [ CommonModule, RouterOutlet],
+  standalone: true
 })
 export class AppComponent {
-  title = 'PS6';
+  title = 'ps6';
+  weatherData: any; // data var
+  exists: boolean = false; // does data exist?
+
+  constructor() {}
+
+  fetchData() {
+    console.log("test!");
+
+    // "fetching" my api data (from a local file)
+    fetch('../assets/apiData.json')
+        .then(response => response.json())
+        .then(jsonData => {
+          this.weatherData = jsonData;
+          this.exists = true;
+        })
+        .catch(error => console.error('you are a fool:', error));
+  }
+
+  doThing() {
+    console.log("DO SOMETHING FOR THE LOVE OF GOD!");
+  }
 }
