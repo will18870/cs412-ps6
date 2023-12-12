bfc90597786aa6cad62f9e2d5935bb30243d68dc
will18870 <will18870@live.com>
2023-12-11 21:31:46 -0500

    made a service based on lecture code

---
+++ src/app/services/opwe.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from "rxjs";
+import {FormGroup} from '@angular/forms';
+import {config as API} from '../config/openweather'
+
+@Injectable({
+    providedIn: 'root'
+})
+export class WxServiceService {
+
+    baseURL: string = API.baseURL
+
+    constructor(private http: HttpClient) {
+    }
+
+    getCurrentWeather(lat: number, lon: number): Promise<any> {
+        const url = `${API.baseURL}?lat=${lat}&lon=${lon}&exclude=minutely,hourly,daily,alerts&appid=${API.apiKey}&units=${API.units}`;
+
+        return this.http.get<any>(url).toPromise();
+    }
+}
